3a397bc56d32d2980561b2bbb3a1bcda82ad4960
윤지상(Jisang Yun)/Cloud Platform팀/SK <jisang@sk.com>
2018-04-09 21:18:59 +0900

    add k8s, Dockerfile

--- awesome-media-apigateway/src/main/java/com/awesome/AwesomeMediaApigatewayApplication.java
+++ awesome-media-apigateway/src/main/java/com/awesome/AwesomeMediaApigatewayApplication.java
@@ -2,10 +2,14 @@ package com.awesome;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableEurekaClient
+@EnableHystrix
 public class AwesomeMediaApigatewayApplication {
 
 	public static void main(String[] args) {
